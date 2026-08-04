5dd6506907db0eef65a27cbfd5f6c7c14d6833b7
Nahkasohva <jokupoku90@gmail.com>
2026-08-04 20:25:15 +0300

    Fixed finally

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
+
 print("Hello world!") 
 print("Hello from feature") 
-=======
+
 print("Hello world!")
->>>>>>> kurssi_eta/main
+
 'print("Hello from feature")' 
 'print("Hello from release")' 
